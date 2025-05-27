db9aa2c5b5c5ff5f759df73a68ea9e559c53559c
Matt Cox <mattcox27@gmail.com>
2025-05-27 14:09:23 +0100

    Removes platform check
    This is unnecessary, as there's nothing that stops it being supported on other platforms.

--- Package.swift
+++ Package.swift
@@ -4,14 +4,6 @@ import PackageDescription
 
 let package = Package(
 	name: "Version",
-	platforms: [
-		.macOS(.v11),
-		.iOS(.v14),
-		.tvOS(.v14),
-		.watchOS(.v7),
-		.macCatalyst(.v14),
-		.visionOS(.v1)
-	],
 	products: [
 		.library(
 			name: "Version",
